d1d58309d56319794e8fc0e1a8b64cad08bf5c89
Arundindu <135002894+Arundindu@users.noreply.github.com>
2024-12-11 17:55:42 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from scripts.constants import app_configuration
-from dotenv import load_dotenv
-
-load_dotenv()
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host=app_configuration.HOST, port=int(app_configuration.PORT), workers=1)
