4eae900c3cd3491a5e8e974952c6bbbbe72599c5
Dindu ArunKumar Reddy <arunkumar.dindu@knowledgelens.com>
2025-11-19 17:24:37 +0530

    test

--- scripts/core/handlers/user_managementhandler.py
+++ scripts/core/handlers/user_managementhandler.py
@@ -274,13 +274,13 @@ class UserManagement:
         # ---------------- PUSH NOTIFICATION TRIGGER ----------------
         try:
             log.info(request_data)
+
             subscription_info = request_data["subscription"]
-            # subscription_info = "DCSR Farms"
 
             payload = json.dumps({
-                "title": "Update Successful",
-                # "body": f"{request_data['key']} updated"
-                "body": f"UPDATE updated"
+                "title": request_data["notificationData"]['title'],
+                "body": f"{request_data['notificationData']['message']} updated"
+                # "body": f"UPDATE updated"
                 # "icon": "/icons/icon-192.png"
             })
 
